03105b4164b087f362f18ae94f452a65c30d447d
adm aquino <admaquino570@gmail.com>
2026-01-09 14:29:53 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cine.curator.pro',
+  appName: 'Cine Curator Pro',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
